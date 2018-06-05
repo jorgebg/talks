c854b7cfa8758039820c14ed7511609f180023f8
Jorge Barata <jorge.barata.gonzalez@gmail.com>
2018-06-05 21:47:24 +0100

    Introduction to Django Rest Framework — End

--- drf-intro/project/user/serializers.py
+++ drf-intro/project/user/serializers.py
@@ -2,16 +2,9 @@ from rest_framework import serializers
 from django.contrib.auth.models import User
 
 
-class UserSerializer(serializers.Serializer):
-    username = serializers.CharField(max_length=200)
-    date_joined = serializers.DateTimeField()
+class UserSerializer(serializers.ModelSerializer):
+    ring_set = serializers.StringRelatedField(many=True)
 
-    def create(self, validated_data):
-        user = User.objects.create(**validated_data)
-        return user
-
-    def update(self, instance, validated_data):
-        instance.username = validated_data.get('username')
-        instance.date_joined = validated_data.get('date_joined')
-        instance.save()
-        return instance
+    class Meta:
+        model = User
+        fields = ('id', 'username', 'ring_set', 'date_joined')
